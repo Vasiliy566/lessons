d5119057c1e6fbb6d7737a00fcfd50015e887bf4
Romanov-v <58908072+Romanov-v@users.noreply.github.com>
2019-12-22 20:21:49 +0300

    Add files via upload

--- C/main.c
+++ C/main.c
@@ -4,45 +4,50 @@
 
 
 
-void randommuf(int desk[3][3], int type ){
-	// найдем свободные клетки и запишем их координаты в массивы Х и У
+void randommuf(int desk[3][3], int type )
+{
+	
 	int n = 0;
 	int x[9];
 	int y[9];
-	for (int i=0; i<3; i++){
-		for (int j=0; j<3; j++){
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
 
-			if (desk[i][j] == 0){
+			if (desk[i][j] == 0)
+			{
 				x[n]=i;
 				y[n]=j;
 				n++;
 			}
 		}
 	}
-	//  нашли свободные клетки
-	// выберем случайную пустую клетку
 	
 
-	int r = (rand() % 10) % n; // !!! Костыль, в macOS %7 возвращает одно и то же !!!
+	int r = (rand() % 10) % n; 
 
-	desk[ x[r] ][ y[r] ] = type;// делаем случайный ход
+	desk[ x[r] ][ y[r] ] = type;/
 
 
 }
 
 
-int inputCoordinates(int desk[3][3], int type){
+int inputCoordinates(int desk[3][3], int type)
+{
 	int x = -1;
 	int y = -1;
 
 	int error_x = scanf("%d", &x);
 	int error_y = scanf("%d", &y);
-	if ( (error_x != 1) || (error_y != 1) ){
+	if ( (error_x != 1) || (error_y != 1) )
+	{
 		printf("incorrect input\n");
 		return 1;
 	}
 
-	if  ( ( x < 0) || ( y < 0) || ( x > 2)|| ( y  > 2) ){
+	if  ( ( x < 0) || ( y < 0) || ( x > 2)|| ( y  > 2) )
+	{
 		printf("error: out of bound! \n");
 		return 1;
 	}
@@ -57,16 +62,23 @@ int inputCoordinates(int desk[3][3], int type){
  return 0;
 }
 
-void watchDesk(int desk[3][3]){
+void watchDesk(int desk[3][3])
+{
 	printf("------------------\n");
-	for (int i=0; i<3; i++){
-		for (int j=0; j<3; j++){
-			if (desk[i][j] == 1){
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
+			if (desk[i][j] == 1)
+			{
 				printf("X ");
 			} else 
-			if (desk[i][j] == -1){
+			if (desk[i][j] == -1)
+			{
 				printf("O ");
-			}else {
+			}
+			else 
+			{
 				printf("- ");
 			}
 		}
@@ -76,35 +88,46 @@ void watchDesk(int desk[3][3]){
 }
 
 
-int cheek_1(int desk[3][3]){
-	for (int i=0; i<3; i++){
-		if( (desk[0][i] == desk[1][i]) && (desk[0][i] == desk[2][i])){
+int cheek_1(int desk[3][3])
+{
+	for (int i=0; i<3; i++)
+	{
+		if( (desk[0][i] == desk[1][i]) && (desk[0][i] == desk[2][i]))
+		{
 			return desk[0][i];
 		}
 	}
 	return 0;
 }
-int cheek_2(int desk[3][3]){
-	for (int i=0; i<3; i++){
-		if( (desk[i][0] == desk[i][1]) && (desk[i][0] == desk[i][2])){
+int cheek_2(int desk[3][3])
+{
+	for (int i=0; i<3; i++)
+	{
+		if( (desk[i][0] == desk[i][1]) && (desk[i][0] == desk[i][2]))
+		{
 			return desk[i][0];
 		}
 	}
 	return 0;
 }
 
-int cheek_3(int desk[3][3]){
+int cheek_3(int desk[3][3])
+{
 	
-	if( (desk[0][0] == desk[1][1]) && (desk[0][0] == desk[2][2])){
+	if( (desk[0][0] == desk[1][1]) && (desk[0][0] == desk[2][2]))
+	{
 		return desk[0][0];
 	}
-	if( (desk[0][2] == desk[1][1]) && (desk[0][2] == desk[2][0])){
+	if( (desk[0][2] == desk[1][1]) && (desk[0][2] == desk[2][0]))
+	{
 		return desk[2][0];
 	}
 	return 0;
 }
 
-int super_check(int desk[3][3]){
+
+int super_check(int desk[3][3])
+{
 	if (cheek_1(desk) != 0)
 		return cheek_1(desk);
 
@@ -116,16 +139,19 @@ int super_check(int desk[3][3]){
 	return 0;
 }
 
-void smartPlay(int desk[3][3], int type, int difficult ){
-	// найдем свободные клетки и запишем их координаты в массивы Х и У
+void smartPlay(int desk[3][3], int type, int difficult )
+{
 	
 	int n = 0;
 	int x[9];
 	int y[9];
-	for (int i=0; i<3; i++){
-		for (int j=0; j<3; j++){
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
 
-			if (desk[i][j] == 0){
+			if (desk[i][j] == 0)
+			{
 				x[n]=i;
 				y[n]=j;
 				n++;
@@ -142,22 +168,27 @@ void smartPlay(int desk[3][3], int type, int difficult ){
 	{
 		int result = 0;
 		
-		for(int j = 0; j < difficult; j ++){
+		for(int j = 0; j < difficult; j ++)
+		{
 			int moves = 9 - n + 1;
 			
 			
 			int type_copy = type;
 			
-			for(int z = 0; z < 3; z ++){
-				for(int k = 0; k < 3; k ++){
+			for(int z = 0; z < 3; z ++)
+			{
+				for(int k = 0; k < 3; k ++)
+				{
 					copyDesk[z][k] = desk[z][k];
 				}
 			}
 			copyDesk[ x[i] ][ y[i] ] = type_copy;
 			type_copy *= -1;
 				
-			while(super_check(copyDesk) == 0){
-				if (moves >= 9){
+			while(super_check(copyDesk) == 0)
+			{
+				if (moves >= 9)
+				{
 					break;
 				}
 				randommuf(copyDesk, type_copy);
@@ -178,7 +209,7 @@ void smartPlay(int desk[3][3], int type, int difficult ){
 	int max = 0;
 	for( int i = 0; i < n; i ++)
 	{
-		//printf("move %d %d rating %d\n", x[i], y[i], results[i]);
+		
 		if (results[i] > results[max])
 			max = i;
 	}
@@ -189,8 +220,10 @@ int main()
 {
 	srand(time(0));
 	int desk[3][3];
-	for (int i=0; i<3; i++){
-		for (int j=0; j<3; j++){
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
 			desk[i][j] = 0;
 		}
 	}
@@ -219,36 +252,42 @@ int main()
 
 	}
 	printf("difficult: %d, %d \n", difficult, h);
-	printf("vyberite storonu\n 1 - kerstiku; -1 - noliki\n");// выбираем уровень сложности
+	printf("vyberite storonu\n 1 - kerstiku; -1 - noliki\n");
 	int user_type ;
 	scanf ("%d", &user_type);
 		watchDesk(desk);
 	while(super_check(desk) == 0){
-		if(type == user_type){
+		if(type == user_type)
+		{
 			error = inputCoordinates(desk, type);
-			//smartPlay(desk, type);
-			//randommuf(desk, type);
-			if(error == 0){
+			
+			if(error == 0)
+			{
 				moves ++;
 			} 
 		} else {
-			if (error == 0){
+			if (error == 0)
+			{
 			smartPlay(desk, type, difficult);
-			//randommuf(desk, type);
+		
 			moves ++;
 			}
 		}
 		type *= -1;
 		watchDesk(desk);
 		
-		if (moves >= 9){
-			if (super_check(desk) == 1){
+		if (moves >= 9)
+		{
+			if (super_check(desk) == 1)
+			{
 				printf("крестики выиграли\n ");
 			}
-			else if (super_check(desk) == -1){
+			else if (super_check(desk) == -1)
+			{
 				printf("нолики выиграли\n ");
 			} else{
-			printf("ничья \n");}
+			printf("ничья \n");
+		}
 			return 0;
 		}
 	}
